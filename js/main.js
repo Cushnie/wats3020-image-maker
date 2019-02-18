ae5c74ec222ebc7ff9ba18d711448a1ff1c06443
Cushnie <cushniecolle@seattleu.edu>
2019-02-18 13:59:09 -0800

    updating js files

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ class ImageMaker {
         // update `this.imagePreview`.
 
         // TODO: Update the `background-image` CSS property for `this.imagePreview`.
-        this.imagePreview.style.backgroundImage = 'url("../images/${this.backgroundInput.value}")';
+        this.imagePreview.style.backgroundImage = 'url("images/${this.backgroundInput.value}")';
 
         // TODO: Update the `innerHTML` of `this.topText`.
         this.topText.innerHTML = this.topTextInput.value;
